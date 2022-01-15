903f78c34744cc874327c6d3a2054e569449067f
Tanuj Sood <tanuj.sood12@gmail.com>
2022-01-15 18:50:09 +0530

    v1.5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import 'antd/dist/antd.min.css';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'antd/dist/antd.min.css';
 import Landing from './Pages/Landing';
 import About from './Pages/About';
 import Error404 from './Pages/Error404';
