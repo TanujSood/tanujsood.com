7434e6267926bd1b1becb654b8837d0669644df0
Tanuj Sood <tanuj.sood12@gmail.com>
2022-01-16 15:25:10 +0530

    v2.4.1

--- src/usePageTracking.js
+++ src/usePageTracking.js
@@ -4,20 +4,11 @@ import ReactGA from "react-ga";
 
 const usePageTracking = () => {
   const location = useLocation();
-  const [initialized, setInitialized] = useState(false);
 
   useEffect(() => {
-    if (!window.location.href.includes("localhost")) {
-      ReactGA.initialize("UA-217268374-1");
-    }
-    setInitialized(true);
-  }, []);
-
-  useEffect(() => {
-    if (initialized) {
-      ReactGA.pageview(location.pathname + location.search);
-    }
-  }, [initialized, location]);
+    ReactGA.initialize("UA-217268374-1");
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location]);
 };
 
 export default usePageTracking;
